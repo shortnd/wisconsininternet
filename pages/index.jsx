3c21937344b3c5da056aaccfac7f7104f9101464
Collin <ococncol@gmail.com>
2020-07-06 00:14:45 -0400

    added recaptcha again...

--- pages/index.jsx
+++ pages/index.jsx
@@ -72,6 +72,7 @@ export default function IndexPage() {
               method="POST"
               action="/success"
               netlify-honeypot="bot-field"
+              data-netlify-recaptcha="true"
               data-netlify="true"
             >
               <input type="hidden" name="contact-us" value="contact" />
@@ -156,6 +157,9 @@ export default function IndexPage() {
                 formik={formik}
                 styles={styles}
               />
+              <div className="mb-3">
+                <div data-netlify-recaptcha="true"></div>
+              </div>
               <div>
                 <button
                   type="submit"
